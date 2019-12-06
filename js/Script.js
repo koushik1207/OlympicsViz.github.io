04b716129b00bfcabe61daab7d24200ef4c74644
Yesha Shah <43424360+yeshashah912@users.noreply.github.com>
2019-12-06 02:23:40 -0800

    Changes for bounce

--- js/Script.js
+++ js/Script.js
@@ -1,9 +1,3 @@
- $(function() {
-    $('.scroll-down').click (function() {
-      $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
-      return false;
-    });
-  });
 
 $( document ).ready(function() {
    // Check breakpoint
